003a760414d9ec1d63df1b092892e7a054cc9913
ohzqq <iamchurkey@gmail.com>
2025-12-25 15:19:14 -0500

    commands

--- cmd/cmd/root.go
+++ cmd/cmd/root.go
@@ -1,27 +1,24 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"os"
 
+	"github.com/ohzqq/webp"
 	"github.com/spf13/cobra"
 )
 
+var opts = webp.Options{
+	Quality: webp.DefaultQuality,
+	Method:  webp.DefaultMethod,
+}
 
+var animate bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cmd",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "webp",
+	Short: "commands for converting and animated WebPs",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -37,15 +34,16 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	rootCmd.PersistentFlags().BoolVarP(&opts.Exact, "exact", "e", false, "Exact preserve the exact RGB values in transparent area.")
+	rootCmd.PersistentFlags().BoolVarP(&opts.Lossless, "lossless", "l", false, "Lossless indicates whether to use the lossless compression. Lossless will ignore quality")
+	rootCmd.PersistentFlags().IntVarP(&opts.Quality, "quality", "q", webp.DefaultQuality, "Quality in the range [0,100]. Quality of 100 implies Lossless. Default is 75.")
+	rootCmd.PersistentFlags().IntVarP(&opts.Method, "method", "m", webp.DefaultMethod, "Method is quality/speed trade-off (0=fast, 6=slower-better). Default is 4.")
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cmd.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().IntVarP(&opts.LoopCount, "loopcount", "c", 0, "Number of times the animation should loop (0 = infinite).")
+	rootCmd.PersistentFlags().IntSliceVarP(&opts.Durations, "durations", "d", []int{webp.DefaultDuration},
+		"Durations is duration per frame in milliseconds. Default is 50. If only one is provided, it'll be the duration for every frame.",
+	)
+	rootCmd.PersistentFlags().IntSliceP(&opts.Disposals, "disposals", []int{0},
+		"Disposal methods after frame display (keep or clear). If only one is provided, it'll be the duration for every frame.",
+	)
 }
-
-
